5123c19d2a1dca86914707da2142a57b6a84b63b
Neon-11 <92382437+Neon-11@users.noreply.github.com>
2023-07-05 22:22:35 +0530

    Update damage_model_2.py

--- damage_model_2.py
+++ damage_model_2.py
@@ -73,6 +73,15 @@ f = damage function
 /beta = regularization term
 """
 
+# hyper-parameter control:
+eq_lambda = 10.0
+damage_lambda = 5.0
+flow_lambda = 10.0
+window_lambda = 10.0
+
+# inferencer controling hyper parameters
+nx = 400
+ny = 1100
 
 """
 Neural Network design:
@@ -85,7 +94,7 @@ Number of hidden layers and number of neurons to decided after tweaking with val
 LE setup:
 
 """
-# asumming 2D: planar stress state
+
 
 # create run; setup the config filesconda 
 @modulus.sym.main(config_path="conf", config_name="config")
@@ -97,18 +106,18 @@ def run(cfg: ModulusConfig) -> None:
 
     u_max = 0.01 # m, say for the sake of example here
 
-    # Using LE for now
+    # asumming 2D: planar stress state
     Equations = Model2D(E = E, nu = nu, lambda_=lambda_, mu = mu, rho = 1, beta = beta, flag = 1, r1 = r1, r2 = r2)
 
     # Generate the sample geometry
     # coordinates for working field
     x, y, t = Symbol("x"), Symbol("y"), Symbol("t")
 
-    panel_origin = (-.025, -.01) # (m, m) left bottom corner
-    panel_dim = (0.05, 0.02) # (m, m)
+    panel_origin = (0, 0) # (m, m) left bottom corner
+    panel_dim = (50e-3, 20e-3) # (m, m)
     panel = Rectangle(panel_origin, (panel_origin[0] + panel_dim[0], panel_origin[1] + panel_dim[1]))
 
-    tri = Triangle(panel_origin, .025, 0.001)  # (base = 25mm & height = 1 mm)
+    tri = Triangle(panel_origin, 25e-3, 1e-3)  # (base = 25mm & height = 1 mm)
 
     thckness = 3e-3 #m
 
@@ -120,11 +129,15 @@ def run(cfg: ModulusConfig) -> None:
     bounds_y = (panel_origin[1], panel_origin[1] + panel_dim[1])
 
     # bound on time scale
-    bounds_t = (0, 1.0)
+    bounds_t = (0.0, 1.0)
+
+    # no. of frames interested in
+    nr_time_windows = 10
 
     # time scales with we are working
-    time_window_size = 0.1
-    nr_time_windows = int(bounds_t[1]/time_window_size)
+    time_window_size = bounds_t[1]
+    
+    # bounds on time ranges
     time_range = {t : bounds_t}
 
     # setp model
@@ -146,12 +159,14 @@ def run(cfg: ModulusConfig) -> None:
             cfg=cfg.arch.fully_connected, # change size from the config.yaml
             activation_fn=Activation.TANH,   
         ) 
-      
+    
+    # generating the neural net
     time_window_net = MovingTimeWindowArch(NN, time_window_size)
 
+    # developing the nodes
     nodes = Equations.make_nodes() + [time_window_net.make_node(name="transient_time_net")]
 
-    # DOMAIN
+    # DOMAINs to be solved
     ic_domain = Domain("Initial_conditions")
     win_domain = Domain("window")
 
@@ -162,10 +177,11 @@ def run(cfg: ModulusConfig) -> None:
             geometry=geo,
             outvar={"traction_x": 0.0, "traction_y": 0.0},
             batch_size = cfg.batch_size.panel_left,
-            criteria = Eq(x, panel_origin[0]) & Eq(x/25 + y, -0.01), # walls where traction is 0
+            criteria = Eq(x, panel_origin[0]), # walls where traction is 0
             parameterization={t: bounds_t, x:panel_origin[0]}, # t is a sympy symbol!!
             batch_per_epoch=500,
         )
+    ic_domain.add_constraint(panel_left, "panel_left")
     win_domain.add_constraint(panel_left, "panel_left")
 
     slant = PointwiseBoundaryConstraint(
@@ -173,10 +189,12 @@ def run(cfg: ModulusConfig) -> None:
             geometry=geo,
             outvar={"traction_x": 0.0, "traction_y": 0.0},
             batch_size = cfg.batch_size.panel_left,
-            criteria = Eq(x/25 + y, -0.01), # walls where traction is 0
-            parameterization={t: bounds_t, x/25 + y: -.01}, # t is a sympy symbol!!
+            criteria = (x + 25*y <= 25e-3),
+            parameterization=time_range,
             batch_per_epoch=500,
         )
+
+    ic_domain.add_constraint(slant, "slant")
     win_domain.add_constraint(slant, "slant")
     
     panel_right = PointwiseBoundaryConstraint(
@@ -188,18 +206,20 @@ def run(cfg: ModulusConfig) -> None:
             parameterization={t: bounds_t, x:(panel_origin[0] + panel_dim[0])}, # parametric terms
             batch_per_epoch=500,
         )
+    ic_domain.add_constraint(panel_right, "panel_right")
     win_domain.add_constraint(panel_right, "panel_right")
 
     # bottom wall
     panel_bottom = PointwiseBoundaryConstraint(
             nodes=nodes,
             geometry=geo,
-            outvar = {"v": 0.0, "d": 0.0},
+            outvar = {"u":0.0, "v": 0.0, "d": 0.0},
             batch_size = cfg.batch_size.panel_bottom,
             criteria = Eq(y, panel_origin[1]) & (x >= panel_origin[0] + panel_dim[0] / 2.0),
-            parameterization={t: bounds_t, y: panel_origin[1]}, # t is a sympy symbol!!
+            parameterization={t: bounds_t, y: panel_origin[1]},
             batch_per_epoch=500,
         )
+    ic_domain.add_constraint(panel_bottom, "panel_bottom")
     win_domain.add_constraint(panel_bottom, "panel_bottom")
 
     # top wall
@@ -214,21 +234,52 @@ def run(cfg: ModulusConfig) -> None:
             batch_per_epoch=500,
             
         )
+    ic_domain.add_constraint(panel_top, "panel_top")
     win_domain.add_constraint(panel_top, "panel_top")
 
+    # setting the intial conditions only in the domain of intial conditions
     # initial condition @t = 0 => d = 0
     IC = PointwiseInteriorConstraint(
         nodes = nodes,
         geometry = geo,
-        outvar={"d": 0.0},
+        outvar={"u": 0.0, "v":0.0, "d": 0.0},
         batch_size=cfg.batch_size.lr_interior,
         # criteria = Eq(t, bounds_t[0]),
-        # bounds={x: bounds_x, y: bounds_y},
-        lambda_weighting={"d": 10.0},
+        bounds={x: bounds_x, y: bounds_y},
+        lambda_weighting={"u": 1.0, "v": 1.0, "d": 1.0},
         parameterization={t : 0.0},
         batch_per_epoch=500,
     )
+    # only intiallly (t=0)
     ic_domain.add_constraint(IC, "IC")
+
+    # make constraint for matching previous windows initial condition
+    # output of n window = input of (n+1) window
+    ic = PointwiseInteriorConstraint(
+        nodes=nodes,
+        geometry=geo,
+        outvar={
+            "u_prev_step_diff": 0,
+            "sigma_xx_prev_step_diff":0, 
+            "sigma_xy_prev_step_diff" : 0, 
+            "sigma_yy_prev_step_diff":0, 
+            "v_prev_step_diff": 0, 
+            "d_prev_step_diff": 0
+        },
+        batch_size=cfg.batch_size.lr_interior,
+        bounds={x: bounds_x, y: bounds_y},
+        lambda_weighting={
+            "u_prev_step_diff": window_lambda,
+            "v_prev_step_diff": window_lambda,
+            "d_prev_step_diff": window_lambda,
+            "sigma_xx_prev_step_diff":window_lambda,
+            "sigma_xy_prev_step_diff":window_lambda,
+            "sigma_yy_prev_step_diff":window_lambda
+        },
+        parameterization={t: 0},
+    )
+    # for any window in consideration
+    win_domain.add_constraint(ic, name="ic")
 
     # constarints to be hold: interior
     Interior = PointwiseInteriorConstraint(
@@ -245,25 +296,25 @@ def run(cfg: ModulusConfig) -> None:
         batch_size=cfg.batch_size.lr_interior,
         bounds={x: bounds_x, y: bounds_y, t: bounds_t},
         lambda_weighting={
-            "equilibrium_x": 10.0,
-            "equilibrium_y": 10.0, # Symbol("sdf") = default
-            "stress_disp_xx": 1.0,
-            "stress_disp_yy": 1.0,
-            "stress_disp_xy": 1.0,
-            "DM": 5.0,
+            "equilibrium_x": eq_lambda,
+            "equilibrium_y": eq_lambda, # Symbol("sdf") = default
+            "stress_disp_xx": flow_lambda,
+            "stress_disp_yy": flow_lambda,
+            "stress_disp_xy": flow_lambda,
+            "DM": damage_lambda,
         },
         parameterization=time_range,
         batch_per_epoch=500,
     )
-    win_domain.add_constraint(Interior, "governing_equations_windows")
-    ic_domain.add_constraint(Interior, "governing_equations_interior")
-
-    nx = 500
-    ny = 200
+    win_domain.add_constraint(Interior, "Interior")
+    ic_domain.add_constraint(Interior, "Interior")
 
     # adding the time_inferencer as we need to store the time varied results
     # add inference data for time slices
-    for i, specific_time in enumerate(np.linspace(0, time_window_size, nr_time_windows)):
+    # below reference:
+    # numpy.linspace(start, stop, num=50, endpoint=True, retstep=False, dtype=None, axis=0)
+    time_array = np.linspace(0, time_window_size, nr_time_windows)
+    for i, specific_time in enumerate(time_array):
         vtk_obj = VTKUniformGrid(
             # give the geometric bounds below
             bounds=[bounds_x, bounds_y],
@@ -282,8 +333,8 @@ def run(cfg: ModulusConfig) -> None:
             invar={"t": np.full([nx*ny, 1], specific_time)},
             batch_size=1000,
         )
-        ic_domain.add_inferencer(grid_inference, name = "time = {} , slice = {}".format(str(specific_time), str(i).zfill(4)))
-        win_domain.add_inferencer(grid_inference, name = "time = {} , slice = {}".format(str(specific_time), str(i).zfill(4)))
+        ic_domain.add_inferencer(grid_inference, name="time_slice_" + str(i).zfill(4) + "_&_actual_time = " + str(time_array[i]))
+        win_domain.add_inferencer(grid_inference, name="time_slice_" + str(i).zfill(4) + "_&_actual_time = " + str(time_array[i]))
 
     # make solver
     slv = SequentialSolver(cfg, [(1, ic_domain), (nr_time_windows, win_domain)], custom_update_operation = time_window_net.move_window)
